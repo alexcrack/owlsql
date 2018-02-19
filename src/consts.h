84a21b6ca852ee914b8430eff6074ce7bfdd999d
Alex_Crack <a13xcrack@gmail.com>
2018-02-19 15:14:49 +0300

    Init src

---
+++ src/consts.h
@@ -0,0 +1,15 @@
+#ifndef CONSTS_H
+#define CONSTS_H
+
+#endif // CONSTS_H
+
+const QString APPNAME           = "OwlSql";
+const QString DEVELOPER         = "Alex_Crack";
+const QString APPDOMAIN         = "https://alexcrack.github.io/owlsql/";
+
+// General things
+const QString REGKEY_SESSIONS   = "Servers";
+const QString CRLF = "#13#10";
+
+// Placeholder text for NULL values
+const QString TEXT_NULL = "(NULL)";
