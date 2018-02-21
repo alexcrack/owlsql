0b1f7fb93b45319d638dac5698b63702135e92fc
Alexey Avramchik <a13xcrack@gmail.com>
2018-02-21 18:14:25 +0300

    Show icons for different DB types. Constructor to create from QJSONObject. Check if inserting child allowed

--- src/session_manager/sessiontreeitem.cpp
+++ src/session_manager/sessiontreeitem.cpp
@@ -6,6 +6,18 @@ SessionTreeItem::SessionTreeItem(const QString &name, TreeItem *parentItem)
 
 }
 
+SessionTreeItem::SessionTreeItem(const QJsonObject &sessionObject, TreeItem *parentItem)
+    : TreeItem(parentItem)
+{
+    m_sessionName = sessionObject.value("name").toString();
+    m_hostName = sessionObject.value("hostName").toString();
+    m_port = sessionObject.value("port").toInt();
+    m_user = sessionObject.value("user").toString();
+    m_password = sessionObject.value("password").toString();
+    m_serverFullVersion = sessionObject.value("serverFullVersion").toString();
+    m_comment = sessionObject.value("comment").toString();
+}
+
 QVariant SessionTreeItem::data(int column) const
 {
     switch(column) {
@@ -13,6 +25,12 @@ QVariant SessionTreeItem::data(int column) const
         return m_sessionName;
     case 1:
         return m_hostName;
+    case 2:
+        return m_serverFullVersion;
+    case 3:
+        return m_user;
+    case 6:
+        return m_comment;
     }
 
     return QVariant();
@@ -21,7 +39,21 @@ QVariant SessionTreeItem::data(int column) const
 QVariant SessionTreeItem::icon(int column) const
 {
     if (column == 0)
-        return QIcon(":/icons/server-percona.png");
+        if (m_serverFullVersion.toLower().contains("percona")) {
+            return QIcon(":/icons/server-percona.png");
+        }
+        else if (m_serverFullVersion.toLower().contains("mariadb")) {
+            return QIcon(":/icons/server-mariadb.png");
+        }
+        else {
+            return QIcon(":/icons/server-mysql.png");
+        }
+
 
     return QVariant();
+}
+
+bool SessionTreeItem::canInsertChild() const
+{
+    return false;
 }
--- src/session_manager/sessiontreeitem.h
+++ src/session_manager/sessiontreeitem.h
@@ -2,14 +2,18 @@
 #define SESSIONTREEITEM_H
 
 #include "treeitem.h"
+#include <QJsonObject>
 
 class SessionTreeItem : public TreeItem
 {
 public:
     explicit SessionTreeItem(const QString &name, TreeItem *parentItem);
+    explicit SessionTreeItem(const QJsonObject &sessionObject, TreeItem *parentItem);
 
     QVariant data(int column) const;
     QVariant icon(int column) const;
+
+    bool canInsertChild() const;
 
 private:
     QString m_sessionName;
@@ -17,6 +21,8 @@ private:
     int m_port;
     QString m_user;
     QString m_password;
+    QString m_serverFullVersion;
+    QString m_comment;
 };
 
 #endif // SESSIONTREEITEM_H
