a1e5c4b2627ad94f80a7762fed1940222fcda0b2
Alexey Avramchik <a13xcrack@gmail.com>
2018-03-02 15:43:21 +0300

    feat(editor): Add editor

---
+++ src/editor/codeeditor.h
@@ -0,0 +1,46 @@
+#ifndef CODEEDITOR_H
+#define CODEEDITOR_H
+
+#include <repository.h>
+
+#include <QPlainTextEdit>
+
+namespace KSyntaxHighlighting {
+class SyntaxHighlighter;
+}
+
+class CodeEditorSidebar;
+
+class CodeEditor : public QPlainTextEdit
+{
+    Q_OBJECT
+public:
+    explicit CodeEditor(QWidget *parent = nullptr);
+    ~CodeEditor();
+
+    void openFile(const QString &fileName);
+
+protected:
+    void contextMenuEvent(QContextMenuEvent *event) Q_DECL_OVERRIDE;
+    void resizeEvent(QResizeEvent *event) Q_DECL_OVERRIDE;
+
+private:
+    friend class CodeEditorSidebar;
+    void setTheme(const KSyntaxHighlighting::Theme &theme);
+    int sidebarWidth() const;
+    void sidebarPaintEvent(QPaintEvent *event);
+    void updateSidebarGeometry();
+    void updateSidebarArea(const QRect &rect, int dy);
+    void highlightCurrentLine();
+
+    QTextBlock blockAtPosition(int y) const;
+    bool isFoldable(const QTextBlock &block) const;
+    bool isFolded(const QTextBlock &block) const;
+    void toggleFold(const QTextBlock &block);
+
+    KSyntaxHighlighting::Repository m_repository;
+    KSyntaxHighlighting::SyntaxHighlighter *m_highlighter;
+    CodeEditorSidebar *m_sideBar;
+};
+
+#endif // CODEEDITOR_H
